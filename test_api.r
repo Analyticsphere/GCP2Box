36d4ce65dd92c0a32d0a388d9898da8f838d9ed5
Jake Peters <jcbptrs@gmail.com>
2023-02-28 11:00:10 -0500

    Update test_api.r

--- test_api.r
+++ test_api.r
@@ -12,7 +12,6 @@ library(bigrquery)
 library(plumber)
 library(boxr)
 library(googleCloudStorageR)
-library(tools)
 
 #* heartbeat...for testing purposes only. Not required to run analysis.
 #* @get /
@@ -25,14 +24,14 @@ function(){return("alive")}
 function() {
 
   # Set parameters 
-  report_name <- "report.pdf"
+  report_name <- "gcp2box_test_report.pdf"
   bucket      <- "gs://test_analytics_bucket_jp" 
-  project     <- "nih-nci-dceg-connect-stg-5519"  
+  project     <- "nih-nci-dceg-connect-dev"  
   billing     <- project # Billing must be same as project
   
   # Simple query.
   query_rec <- "SELECT d_117249500 AS Age 
-                FROM `nih-nci-dceg-connect-stg-5519.FlatConnect.participants_JP` 
+                FROM `nih-nci-dceg-connect-dev.FlatConnect.participants_JP` 
                 WHERE Connect_ID IS NOT NULL"
   
   # BigQuery authorization. Should work smoothly on GCP without any inputs.
@@ -43,15 +42,11 @@ function() {
   rec_data  <- bq_table_download(rec_table, bigint = "integer64")
   
   # Write a table to pdf as an example "report". 
-  # Add time stamp to report name
-  report_fid <- paste0(file_path_sans_ext(report_name),
-                       format(Sys.time(), "_%m_%d_%Y"),
-                       ".", file_ext(report_name))
-  pdf(report_name)    # Opens a PDF
+  pdf(report_name)   # Opens a PDF
   hist(rec_data$Age) # Write histogram to PDF
   dev.off()          # Closes PDF
   
-  # Authenticate with Box and write report pdf to Box folder 
+  # TODO: Authenticate with Box and write report pdf to Box folder 
   boxr::box_auth_service(token_file = NULL, token_text = NULL)
   boxr::box_write(object, file_name, dir_id = box_getwd(), description = NULL)
  
@@ -59,7 +54,7 @@ function() {
   scope <- c("https://www.googleapis.com/auth/cloud-platform")
   token <- token_fetch(scopes=scope)
   gcs_auth(token=token)
-  gcs_upload(report_fid, bucket=bucket, name=report_fid) 
+  gcs_upload(report_name, bucket=bucket, name=report_name) 
   
   # Return a string for for API testing purposes
   ret_str <- paste("All done. Check", bucket, "for", report_fid)
